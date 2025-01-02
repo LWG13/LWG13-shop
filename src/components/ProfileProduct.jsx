8991f9bab25703beff0a2d1300729ca27354ffae
LWG13 <164619164+LWG13@users.noreply.github.com>
2025-01-02 22:03:23 +0700

    Update ProfileProduct.jsx

--- src/components/ProfileProduct.jsx
+++ src/components/ProfileProduct.jsx
@@ -41,7 +41,6 @@ export default function ProfileProduct() {
        <button className={page === 0 ? "pagButton none" : "pagButton"} onClick={() => setPage(page - 1)} >{page === 0 ? 0 : page - 1}</button>
         <button className="pagButton now">{page}</button>
          <button onClick={() => setPage(page + 1)} className={data?.data.length !== 20 ? "pagButton none" : "pagButton"}>{page + 1}</button>
-        <button onClick={() => setPage(page + 2)} className={data?.data.length !== 20 ? "pagButton none" : "pagButton"}>{page + 2}</button>
       </div>
       )}
      
